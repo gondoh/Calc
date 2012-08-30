ba8a8700a67a699af1f34393b5621bbede259b82
gondoh <gondoh@gufii.net>
2012-08-30 21:59:27 +0900

    計算機基本機能を実装したよ

--- src/sample/application/calculator/CalculatorActivity.java
+++ src/sample/application/calculator/CalculatorActivity.java
@@ -1,5 +1,6 @@
 package sample.application.calculator;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 import android.os.Bundle;
@@ -9,10 +10,13 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalculatorActivity extends Activity {
 	
 	private String strTemp;
+	private String strResult;
+	private Integer operator;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,8 +68,65 @@ public class CalculatorActivity extends Activity {
     	((TextView)this.findViewById(R.id.displayPanel)).setText(fText);
     }
     
+    public void functionKeyOnClick(View v){
+    	switch (v.getId()) {
+		case R.id.keypadAC:
+			break;
+
+		default:
+			break;
+		}
+    }
     
+    public void operatorKeyOnClick(View v){
+    	if(this.operator != 0){
+    		if(this.strTemp.length() > 0){
+    			this.showNumber(this.strResult);
+    		}
+    	}else{
+    		if(this.strTemp.length() > 0){
+    			this.strResult = this.strTemp;
+    		}
+    	}
+    	
+    	this.strTemp = "";
+    	
+    	if(v.getId() == R.id.keypadEq){
+    		this.operator = 0;
+    	}else{
+    		this.operator = v.getId();
+    	}
+    }
     
+    private String doCalc(){
+    	BigDecimal bd1 = new BigDecimal(this.strResult);
+    	BigDecimal bd2 = new BigDecimal(this.strTemp);
+    	BigDecimal result = BigDecimal.ZERO;
+    	switch (this.operator) {
+		case R.id.keypadAdd:
+			result = bd1.add(bd2);
+			break;
+		case R.id.keypadSub:
+			result = bd1.subtract(bd2);
+			break;
+		case R.id.keypadMulti:
+			result = bd1.multiply(bd2);
+			break;
+		case R.id.keypadDiv:
+			if (!bd2.equals(BigDecimal.ZERO)) {
+				result = bd1.divide(bd2, 12, 3);
+			}else{
+				Toast toast = Toast.makeText(this,  R.string.toast_div_by_zero,  1000);
+				toast.show();
+			}
+			break;
+		}
+    	
+    	if(result.toString().indexOf(".") >= 0){
+    		return result.toString().replaceAll("¥¥.0+$|0+$", "");
+    	}
+    	return result.toString();
+    }
     
 //    public void numKeyOnClick(View v) {
 //    	String strInKey = (String) ((Button)v).getText();
